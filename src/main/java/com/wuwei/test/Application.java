9859596599d94e5b1b98371769806bef48ab820b
wuwei <wuwei@6891@gmail.com>
2017-08-06 21:00:32 +0800

    添加Mybatis ORM对数据库的映射

--- src/main/java/com/wuwei/test/Application.java
+++ src/main/java/com/wuwei/test/Application.java
@@ -6,14 +6,15 @@ package com.wuwei.test;
  * @author 吴维
  * @date 2017-8-5 14:04:37
  */
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.wuwei.test.dao")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
--- src/main/java/com/wuwei/test/controller/Controller.java
+++ src/main/java/com/wuwei/test/controller/Controller.java
@@ -4,7 +4,6 @@ import com.wuwei.test.entity.Student;
 import com.wuwei.test.service.StudentService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
