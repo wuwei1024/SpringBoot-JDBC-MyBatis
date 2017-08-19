fd5297f2d215e4aeb29c071aeefe28de7577efae
wuwei <wuwei6891@gmail.com>
2017-08-19 11:05:25 +0800

    添加日志记录配置文件

--- src/main/java/com/wuwei/Application.java
+++ src/main/java/com/wuwei/Application.java
@@ -1,15 +1,15 @@
 package com.wuwei;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * SpringBoot入口程序
  *
  * @author 吴维
  * @date 2017-8-5 14:04:37
  */
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 @MapperScan(basePackages = "com.wuwei.dao")
 public class Application {
